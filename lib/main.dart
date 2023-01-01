f1aa8246d4f849203d8cb95b37328f930e406ada
jeminblack <jeminthesiya101@gmail.com>
2023-01-01 23:22:19 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:colors_change/color.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: color(),
+    ),
+  );
+}
